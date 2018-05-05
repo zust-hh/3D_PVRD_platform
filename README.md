f8e680aa9e7f5a27cafe7a36fdaa70982d647f40
hhhuhao_i <hhhuhao_i@didichuxing.com>
2018-05-05 15:05:44 +0800

    提交

--- README.md
+++ README.md
@@ -3,7 +3,10 @@
 3D全景创业资源对接平台(3D_panoramic_venture_resources_docking_platform)
 
 PC_management: 后台管理代码
+
 WX_client: WX小程序端代码
+
+server: node后台代码
 
 ## PC_management
 
@@ -28,4 +31,4 @@ wepy build --watch
 * image标签可以加载本地图片，但背景图片用网络图片或者base64
 * 组件props无法传数组，在组件中拼接，希望后台可以把类似涉及行业这种拼接好直接返回字符串
 * 微信获取来的头像可能需要存在本地一下，方便评论头像显示
-* 聊一聊界面仿照微信需要一个封面图字段
+* 申请投资，需要和后台沟通需要什么字段
---
+++ server 2/app.js
@@ -0,0 +1,43 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var cors = require('cors');
+
+var usersRouter = require('./routes/users');
+var serverRouter = require('./routes/server');
+
+var app = express();
+app.use(logger('dev'));
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors({
+  origin:['http://127.0.0.1:3000'],
+  methods:['GET','POST'],
+  allowedHeaders:['Content-Type', 'Authorization']
+}));
+
+app.use('/users', usersRouter);
+app.use('/server', serverRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+app.listen(8818,function(req,res){
+  console.log("已开启服务，端口为8818")
+});
+module.exports = app;
