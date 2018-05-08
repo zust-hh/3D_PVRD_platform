025334d7afb473ef60d02d8dfc85114bd8717ae3
tosim <82415327@qq.com>
2018-05-08 17:59:05 +0800

    修改sql文件

--- sql/xinmiao_mini.sql
+++ sql/xinmiao_mini.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50721
  File Encoding         : 65001
 
- Date: 05/05/2018 16:58:58
+ Date: 08/05/2018 17:58:39
 */
 
 SET NAMES utf8mb4;
@@ -26,9 +26,9 @@ CREATE TABLE `apply`  (
   `apply_user` int(11) NOT NULL COMMENT '申请人',
   `apply_company_id` varchar(11) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '申请的投资机构',
   `apply_status` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '审核状态',
-  `is_read` int(11) UNSIGNED NOT NULL DEFAULT 0,
+  `is_read` int(11) UNSIGNED NOT NULL DEFAULT 0 COMMENT '是否被阅读',
   PRIMARY KEY (`apply_id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for comment
@@ -36,10 +36,10 @@ CREATE TABLE `apply`  (
 DROP TABLE IF EXISTS `comment`;
 CREATE TABLE `comment`  (
   `comment_id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `comment_user_id` int(11) NOT NULL,
-  `comment_content` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `comment_company_id` int(11) NOT NULL,
-  `coment_time` datetime(0) NOT NULL,
+  `comment_user_id` int(11) NOT NULL COMMENT '评论者的id',
+  `comment_content` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '评论内容',
+  `comment_company_id` int(11) NOT NULL COMMENT '被评论的公司',
+  `coment_time` datetime(0) NOT NULL ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '评论时间',
   PRIMARY KEY (`comment_id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
@@ -49,17 +49,17 @@ CREATE TABLE `comment`  (
 DROP TABLE IF EXISTS `company`;
 CREATE TABLE `company`  (
   `company_id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `company_name` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `company_name` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '公司名',
   `company_description` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `company_address` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `company_tags` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `book_img` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `company_tags` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '公司类型',
+  `book_img` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '公司封面图',
   `company_ceo` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `company_url` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `company_type` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
+  `company_type` int(11) NOT NULL COMMENT '公司类型1是创业，2是投资',
+  `user_id` int(11) NOT NULL COMMENT '关联的用户id',
   PRIMARY KEY (`company_id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for img
@@ -67,9 +67,9 @@ CREATE TABLE `company`  (
 DROP TABLE IF EXISTS `img`;
 CREATE TABLE `img`  (
   `img_id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `img_url` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `img_pos` int(11) NOT NULL,
-  `scene_id` int(11) NOT NULL,
+  `img_url` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '在项目目录下的存储路径',
+  `img_pos` int(11) NOT NULL COMMENT '在六张图片中的位置',
+  `scene_id` int(11) NOT NULL COMMENT '场景id',
   PRIMARY KEY (`img_id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 7 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
@@ -79,8 +79,8 @@ CREATE TABLE `img`  (
 DROP TABLE IF EXISTS `in_apply`;
 CREATE TABLE `in_apply`  (
   `in_apply_id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `in_apply_user_id` int(11) NOT NULL,
-  `in_apply_status` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `in_apply_user_id` int(11) NOT NULL COMMENT '申请人的id',
+  `in_apply_status` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '申请状态',
   PRIMARY KEY (`in_apply_id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
@@ -101,9 +101,9 @@ CREATE TABLE `permission`  (
 DROP TABLE IF EXISTS `scene`;
 CREATE TABLE `scene`  (
   `scene_id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `scene_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `company_id` int(11) NOT NULL,
-  `scene_num` int(11) NOT NULL,
+  `scene_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '场景名字',
+  `company_id` int(11) NOT NULL COMMENT '对应的公司id',
+  `scene_num` int(11) NOT NULL COMMENT '场景的序号',
   PRIMARY KEY (`scene_id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
@@ -113,12 +113,12 @@ CREATE TABLE `scene`  (
 DROP TABLE IF EXISTS `sub_comment`;
 CREATE TABLE `sub_comment`  (
   `sub_comment_id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `comment_id` int(11) NOT NULL,
+  `comment_id` int(11) NOT NULL COMMENT '对应主评论的id',
   `user_a_name` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `user_b_name` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `sub_comment_content` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `sub_comment_time` datetime(0) NOT NULL ON UPDATE CURRENT_TIMESTAMP(0),
-  `is_read` int(11) NOT NULL DEFAULT 0,
+  `is_read` int(11) NOT NULL DEFAULT 0 COMMENT '是否被读',
   PRIMARY KEY (`sub_comment_id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
@@ -128,12 +128,12 @@ CREATE TABLE `sub_comment`  (
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user`  (
   `user_id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `user_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '',
-  `user_passwd` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '',
-  `user_wx` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '',
-  `user_type` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '',
-  `token` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '',
-  `user_icon` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `user_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT '' COMMENT '后台用户名',
+  `user_passwd` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT '' COMMENT '后台登录密码',
+  `user_wx` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT '',
+  `user_type` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT '' COMMENT '用户类型，字符串',
+  `token` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT '',
+  `user_icon` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT '' COMMENT '微新头像',
   PRIMARY KEY (`user_id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
