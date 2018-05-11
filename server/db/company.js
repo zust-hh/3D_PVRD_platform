e9410b4d663c321b59b98bee936dfde5250707ca
wangguoqing123 <544520279@qq.com>
2018-05-11 20:46:04 +0800

    完善后台

--- server/db/company.js
+++ server/db/company.js
@@ -7,7 +7,7 @@ var companysql = {
   insert:'INSERT INTO company(id,username,password,name) VALUES(?,?,?,?)', 
   queryAll:'SELECT * FROM company',
   //通过公司id查找公司  
-  getCompanyById:'SELECT * FROM company WHERE id = ?',
+  getCompanyById:'SELECT * FROM company WHERE company_id = ?',
   //公司申请,同意为1，拒绝为2
   companyapply:'UPDATE company SET company_tags = ? WHERE company_id = ? ',
   //删除公司
